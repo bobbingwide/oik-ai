a05a937e44addccd84c40b239f3ba56961f3b881
Herb Miller <herb@bobbingwide.com>
2024-01-18 22:22:42 +0000

    Add image() and image_data() methods. #3

--- classes/class-Oik-AI.php
+++ classes/class-Oik-AI.php
@@ -88,4 +88,66 @@ class Oik_AI {
         return $this->result;
     }
 
+	/**
+	 * Generate an image using DALL-E-3
+	 *
+	 *  https://platform.openai.com/docs/guides/images/usage?context=node
+	 * https://platform.openai.com/docs/api-reference/images/create
+	 *
+	 * - size: 1024x1024, 1024x1792 or 1792x1024 pixels
+	 * - n: You can request 1 image at a time with DALL·E 3
+	 * - quality: 'standard' or 'hd'
+	 *
+	 * With the release of DALL·E 3, the model
+	 * - now takes in the default prompt provided
+	 * - and automatically re-write it for safety reasons,
+	 * - and to add more detail (more detailed prompts generally result in higher quality images).
+	 *
+	 * While it is not currently possible to disable this feature,
+	 * you can use prompting to get outputs closer to your requested image
+	 * by adding the following to your prompt:
+	 *
+	 * I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS:.
+	 *
+	 * Each image can be returned as either a URL or Base64 data, using the response_format parameter.
+	 * URLs will expire after an hour.
+	 *
+	 * style: 'vivid' or 'natural'
+	 *
+	 * The style of the generated images. Must be one of vivid (default) or natural.
+	 * Vivid causes the model to lean towards generating hyper-real and dramatic images.
+	 * Natural causes the model to produce more natural, less hyper-real looking images.
+	 * This param is only supported for dall-e-3
+	 *
+	 * @return mixed
+	 */
+	function image() {
+		$result = $this->client->images()->create([
+			'model' => 'dall-e-2',
+			'prompt' => $this->system_message,
+			'n' => 1,
+			'size' => '1024x1024',
+			'response_format' => 'url',
+		]);
+		print_r( $result );
+		$url = $result->data[0]['url'];
+		return $url;
+	}
+
+	function image_data() {
+		$result = $this->client->images()->create([
+			'model' => 'dall-e-3',
+			'prompt' => $this->system_message,
+			'quality' => 'hd',
+			'n' => 1,
+			'size' => '1792x1024',
+			'response_format' => 'b64_json',
+		]);
+		//print_r( $result );
+		$image = $result->data[0]['b64_json'];
+
+		return $image;
+
+	}
+
 }
